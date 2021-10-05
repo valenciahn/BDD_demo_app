fbcc3e93727d9612c0923160ecb1b473701a2ecc
Nestor <valenciahn@gmail.com>
2021-10-05 11:45:01 -0500

    se agrega configuracion para reportes cucumber en formato json

--- src/test/java/org/example/acceptancetests/DemoTestSuit.java
+++ src/test/java/org/example/acceptancetests/DemoTestSuit.java
@@ -5,6 +5,6 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/demo", glue="org.example.steps")
+@CucumberOptions(features="src/test/resources/features/demo", glue="org.example.steps",plugin = { "usage", "json:target/site/cucumber.json" })
 public class DemoTestSuit {
 }
